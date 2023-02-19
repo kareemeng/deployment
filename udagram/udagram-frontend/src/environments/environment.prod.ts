4eab29bdbf6fe98b150c86cabd59215749a4552b
Kareem Ahmed <kareemahmed66@outlook.com>
2023-02-19 18:13:30 +0200

    fix 2

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: `Udagram`,
-  apiHost: `${process.env.URL}/api/v0`,
+  apiHost: `kareem-env1.eba-pv6hup3t.us-east-1.elasticbeanstalk.com/api/v0`,
 };
 
 /*
